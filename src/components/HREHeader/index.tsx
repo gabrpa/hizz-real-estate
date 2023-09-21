ee5d125306b0a6db227fe8b776abcddce538ffc8
Gabriel Paulino <gabriel.paulino@lambda-logos.co>
2023-09-21 18:53:00 +0900

    changed header to jpg

--- src/components/HREHeader/index.tsx
+++ src/components/HREHeader/index.tsx
@@ -3,7 +3,7 @@ import HREButton from '../atoms/HREButton'
 import HRETitle from '../atoms/HRETitle'
 import HRESubtitle from '../atoms/HRESubtitle'
 import HREBox from '../atoms/HREBox'
-import Image from './header.png'
+import Image from './banner2.jpg'
 
 export default function HREHeader() {
   return (  
